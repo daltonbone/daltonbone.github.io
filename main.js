7115b88a2ba62227488693862edfcd731b03f49d
daltonbone <daltonbone@gmail.com>
2019-01-25 21:28:14 -0600

    idk what im doing

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function saveToFirebase(email) {
         email: email
     };
 
-    firebase.database().ref('subscription-entries').push().set(emailObject)
+    firebase.database().ref('forums').push().set(emailObject)
         .then(function(snapshot) {
             success(); // some success method
         }, function(error) {
