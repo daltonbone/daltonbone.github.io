c581b55c5aaa851f21bd78838fc803843e0c37fd
daltonbone <daltonbone@gmail.com>
2019-01-25 21:18:43 -0600

    bigger update

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 function openNav(){
-    
-    document.getElementById("mySidenav").style.width = "250px";    
+
+    document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav(){
-    
+
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+function saveToFirebase(email) {
+    var emailObject = {
+        email: email
+    };
+
+    firebase.database().ref('subscription-entries').push().set(emailObject)
+        .then(function(snapshot) {
+            success(); // some success method
+        }, function(error) {
+            console.log('error' + error);
+            error(); // some error method
+        });
+}
+
+saveToFirebase(email);
